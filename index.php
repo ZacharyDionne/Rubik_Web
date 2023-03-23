3511f627c58c1c5a6083b6c1b62c2f0c7bb379b4
Zachary Dionne <1940887@techinfotr.qc.ca>
2023-03-23 13:27:44 -0400

    Template en cours...

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet Rubik</title>
+    <link rel="icon" href="rubik.ico">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script defer src="js/bootstrap.bundle.min.js"></script>
+    <script defer src="js/index.js"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6 text-center">
+                colonne 1
+            </div>
+            <div class="col-6">
+            colonne 2
+            </div>
+        </div>
+    </div>
+    <footer>
+        <a href="https://www.flaticon.com/free-icons/rubik" title="Rubik icons">Rubik icons created by Creaticca Creative Agency - Flaticon</a>
+    </footer>
+</body>
+</html>
